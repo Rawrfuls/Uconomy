6d32a3e33f8d372126c44b77a0f74ba9e34aebf2
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-07-27 02:37:25 +0200

    Fixing config

--- UconomyConfiguration.cs
+++ UconomyConfiguration.cs
@@ -8,32 +8,14 @@ namespace unturned.ROCKS.Uconomy
 {
     public class UconomyConfiguration : IRocketPluginConfiguration
     {
-        public string DatabaseAddress;
-        public string DatabaseUsername;
-        public string DatabasePassword;
-        public string DatabaseName;
-        public string DatabaseTableName;
-        public int DatabasePort;
+        public string DatabaseAddress = "localhost";
+        public string DatabaseUsername = "unturned";
+        public string DatabasePassword = "password";
+        public string DatabaseName = "unturned";
+        public string DatabaseTableName = "uconomy";
+        public int DatabasePort = 3306;
 
-        public decimal InitialBalance;
-        public string MoneyName;
-
-        public IRocketPluginConfiguration DefaultConfiguration
-        {
-            get
-            {
-                UconomyConfiguration config = new UconomyConfiguration();
-                config.DatabaseAddress = "localhost";
-                config.DatabaseUsername = "unturned";
-                config.DatabasePassword = "password";
-                config.DatabaseName = "unturned";
-                config.DatabaseTableName = "uconomy";
-                config.DatabasePort = 3306;
-
-                config.InitialBalance = 30;
-                config.MoneyName = "Credits";
-                return config;
-            }
-        }
+        public decimal InitialBalance = 30;
+        public string MoneyName = "Credits";
     }
 }
